72f74228b668b48d59e78a86ffbe6c2f51da3be5
ipetrash <ilya.petrash@inbox.ru>
2021-06-16 21:49:17 +0500

    Добавлена человекочитаемая ошибка при отсутствии токена для бота

--- config.py
+++ config.py
@@ -5,12 +5,22 @@ __author__ = 'ipetrash'
 
 
 import os
+import sys
+
 from pathlib import Path
 
 
 DIR = Path(__file__).resolve().parent
 TOKEN_FILE_NAME = DIR / 'TOKEN.txt'
 
-TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
+try:
+    TOKEN = os.environ.get('TOKEN') or TOKEN_FILE_NAME.read_text('utf-8').strip()
+    if not TOKEN:
+        raise Exception('TOKEN пустой!')
+
+except:
+    print(f'Нужно в {TOKEN_FILE_NAME.name} или в переменную окружения TOKEN добавить токен бота')
+    TOKEN_FILE_NAME.touch()
+    sys.exit()
 
 ERROR_TEXT = '⚠ Возникла какая-то проблема. Попробуйте повторить запрос или попробовать чуть позже...'
